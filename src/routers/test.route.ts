6894563d738ee7a724562ec0bfee7d2dc6de956d
finprojcwdol <finprojcwdol@gmail.com>
2025-07-18 07:00:30 +0000

    authentication

--- src/routers/test.route.ts
+++ src/routers/test.route.ts
@@ -5,7 +5,7 @@ import { authenticateToken } from '../middlewares/auth';
 const router = Router();
 
 // Terapkan middleware otentikasi
-router.get('/test-prisma', authenticateToken, testPrismaConnection);
-// router.get('/test-prisma', testPrismaConnection);
+// router.get('/test-prisma', authenticateToken, testPrismaConnection);
+router.get('/test-prisma', testPrismaConnection);
 
 export default router;
