a35649a6d362f8fbf2a44ed7047be46ba67c7927
finprojcwdol <finprojcwdol@gmail.com>
2025-07-18 06:54:34 +0000

    clean unused

--- src/routers/test.route copy.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { testPrismaConnection } from '../controllers/prismaTest.controller';
-
-const router = express.Router();
-
-router.get('/test-prisma', testPrismaConnection);
-
-
-export default router;
